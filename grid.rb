204ba330d2f1f464e9affb2ca34285860d0928c0
Ben Blue <bencblue@gmail.com>
2023-01-14 10:43:02 -0800

    Grid abstraction

---
+++ grid.rb
@@ -0,0 +1,121 @@
+class Grid
+  include Enumerable
+
+  def initialize(r_max, c_max, node_class = Node)
+    @node_class = node_class
+    @grid = r_max.times.map { Array.new(c_max) }
+
+    if block_given?
+      r_max.times do |r|
+        c_max.times do |c|
+          yield self, r, c
+        end
+      end
+    end
+  end
+
+  def rows
+    @grid
+  end
+
+  def each
+    @grid.each do |r|
+      r.each do |c|
+        yield c
+      end
+    end
+  end
+
+  def put(r, c, value)
+    @grid[r][c] = @node_class.new(r, c, value, self)
+  end
+
+  def tl(node)
+    n(node.r-1, node.c-1) unless node.r == 0 || node.c == 0
+  end
+
+  def t(node)
+    n(node.r-1, node.c) unless node.r == 0
+  end
+
+  def tr(node)
+    n(node.r-1, node.c+1) unless node.c == c_max || node.r == 0
+  end
+
+  def r(node)
+    n(node.r, node.c+1) unless node.c == c_max
+  end
+
+  def br(node)
+    n(node.r+1, node.c+1) unless node.c == c_max || node.r == r_max
+  end
+
+  def b(node)
+    n(node.r+1, node.c) unless node.r == r_max
+  end
+
+  def bl(node)
+    n(node.r+1, node.c-1) unless node.r == r_max || node.c == 0
+  end
+
+  def l(node)
+    n(node.r, node.c-1) unless node.c == 0
+  end
+
+  def n(r, c)
+    @grid[r][c]
+  end
+
+  def r_max
+    @r_max ||= @grid.length - 1
+  end
+
+  def c_max
+    @c_max ||= @grid[0].length - 1
+  end
+
+  def neighbors_90(node)
+    neighbors = [t(node), r(node), b(node), l(node)]
+    neighbors.compact!
+    neighbors
+  end
+
+  def parse_raw(raw)
+    g = []
+    raw.each_with_index do |row, ri|
+      g << [] 
+      row.chars.each_with_index do |v, ci|
+        g[ri] << @node_class.new(ri, ci, v, self)
+      end
+    end
+    g
+  end
+
+  def print_grid(r_range: 0..r_max, c_range: 0..c_max)
+    @grid.slice(r_range).each do |r|
+      r.slice(c_range).each do |c|
+        print "#{c.v}"
+      end
+      print "\n"
+    end
+  end
+
+  class Node
+    attr_accessor :r, :c, :v, :g
+  
+    def initialize(r,c,v,g)
+      @r = r
+      @c = c
+      @v = v
+      @g = g
+    end
+  
+    def inspect
+      "<#{self.class} row=#{r} col=#{c} value=#{v}>"
+    end
+  
+    def to_s
+      inspect
+    end
+  end
+end
